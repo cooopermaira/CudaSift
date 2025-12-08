aaebeef508498eadeb98db35328def685b22dfc3
pathcam <cmaira@tulane.edu>
2025-12-08 16:57:00 -0600

    updates for pathcam

--- cudaSiftH.cu
+++ cudaSiftH.cu
@@ -28,12 +28,11 @@ void InitCuda(int devNum)
   deviceInit(devNum);  
   cudaDeviceProp prop;
   cudaGetDeviceProperties(&prop, devNum);
-  printf("Device Number: %d\n", devNum);
-  printf("  Device name: %s\n", prop.name);
-  printf("  Memory Clock Rate (MHz): %d\n", prop.memoryClockRate/1000);
-  printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
-  printf("  Peak Memory Bandwidth (GB/s): %.1f\n\n",
-	 2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+  // printf("Device Number: %d\n", devNum);
+  // printf("  Device name: %s\n", prop.name);
+  // printf("  Memory Clock Rate (MHz): %d\n", prop.memoryClockRate/1000);
+  // printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
+  // printf("  Peak Memory Bandwidth (GB/s): %.1f\n\n",2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
 }
 
 float *AllocSiftTempMemory(int width, int height, int numOctaves, bool scaleUp)
